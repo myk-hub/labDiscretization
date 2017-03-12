890261c3d9c8083c1eebc6847b28dd4d4caac089
usernamen7 <n7username7@gmail.com>
2017-03-12 16:35:06 +0200

    work with DOM

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-var xArr = [1, 2, 3];
-var yArr = [1, 3, 5];
-// x = 1,2,3; y = 1,3,5
-// cool function you are looking for
+var xArr = [1, 2, 3],
+    yArr = [1, 3, 5];
+
 function discretization(x, y) {
-  var y = y.reverse(); // реверс бо на тих картинках шо ти кидала, там Y йде в спадаючій від 3 до 1
-      results = []; // тут збираєм результати
-      sum = 0; // змінна, де зберігаємо суму кожної ітерації
-      sLength = x.length + y.length - 1 // це розмір вихідної послідовності,
-                                      // сума довжин масивів -1, надіюсь це понятно
+    y = y.reverse();
+    var  results = [];
+    var  sum = 0;
+    var  sLength = x.length + y.length - 1;
+
   for(var m = 0; m < sLength; ++m){
       for(var k = 0; k < sLength; ++k){
-          if(x[k] != undefined && y[m-k] != undefined){  // тут ми типу пропускаємо сумування, якщо одна з клітинок пуста
+          if(x[k] != undefined && y[m-k] != undefined){
               sum += x[k] * y[m-k];
           }
       }
@@ -20,6 +19,22 @@ function discretization(x, y) {
   return results;
 }
 
+console.log(discretization(xArr, yArr));
+
 window.onload = function(){
     document.getElementById('result').innerHTML = discretization(xArr, yArr);
+}
+
+
+function getData() {
+    var x = document.getElementById('xArray').value;
+    var y = document.getElementById('yArray').value;
+    x = x.split(',');
+    y = y.split(',');
+    document.getElementById('try').innerHTML = x;
+}
+
+
+window.onload = function(){
+    document.getElementById('buttonResult').addEventListeneer('click', getData);
 }
