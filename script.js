a813a1e2338b5dc951e633c2d82c91ac8c5d2cfe
usernamen7 <n7username7@gmail.com>
2017-03-12 18:29:50 +0200

    nothing to do

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-var xArr = [1, 2, 3],
-    yArr = [1, 3, 5];
+function getData(x, y) {
+    var xArr = document.getElementById('xArray').value;
+    var yArr = document.getElementById('yArray').value;
+        xArr = xArr.split(',');
+        yArr = yArr.split(',');
+    document.getElementById('try').innerHTML = xArr;
+    //    var xArr = [1, 2, 3],
+    //        yArr = [1, 3, 5];
+    console.log(typeof(xArr));
+    console.log((xArr));
+    console.log(typeof(yArr));
+}
 
 function discretization(x, y) {
-    y = y.reverse();
-    var  results = [];
-    var  sum = 0;
-    var  sLength = x.length + y.length - 1;
+    var y = y.reverse();
+    var results = [];
+    var sum = 0;
+    var sLength = x.length + y.length - 1;
 
-  for(var m = 0; m < sLength; ++m){
-      for(var k = 0; k < sLength; ++k){
-          if(x[k] != undefined && y[m-k] != undefined){
-              sum += x[k] * y[m-k];
-          }
-      }
-      results.push(sum);
-      sum = 0;
-  }
-  return results;
+    for (var m = 0; m < sLength; ++m) {
+        for (var k = 0; k < sLength; ++k) {
+            if (x[k] != undefined && y[m - k] != undefined) {
+                sum += x[k] * y[m - k];
+            }
+        }
+        results.push(sum);
+        sum = 0;
+    }
+    return results;
 }
 
-console.log(discretization(xArr, yArr));
-
-window.onload = function(){
-    document.getElementById('result').innerHTML = discretization(xArr, yArr);
-}
-
-
-function getData() {
-    var x = document.getElementById('xArray').value;
-    var y = document.getElementById('yArray').value;
-    x = x.split(',');
-    y = y.split(',');
-    document.getElementById('try').innerHTML = x;
-}
-
-
-window.onload = function(){
-    document.getElementById('buttonResult').addEventListeneer('click', getData);
+window.onload = function() {
+    document.getElementById('result').innerHTML = getData(xArr, yArr);
 }
