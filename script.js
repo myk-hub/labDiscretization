9844368218895132bad1bbcf44cb73031de48475
usernamen7 <n7username7@gmail.com>
2017-03-14 17:23:38 +0200

    code optimization

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-var button = document.getElementById('button'),
-    xArr = [],
-    yArr = [],
-    results = [];
-
 document.getElementById('button').onclick = function() {
-        xArr = document.getElementById('xArray').value.split(',');
-        yArr = document.getElementById('yArray').value.split(',');
-        discretization(xArr, yArr);
+    var xArr = document.getElementById('xArray').value.split(','),
+        yArr = document.getElementById('yArray').value.split(',').reverse();
+    discretization(xArr, yArr);
 };
 
 function discretization(x, y) {
-    var y = y.reverse();
-        results = [];
-        sum = 0;
-        sLength = x.length + y.length - 1;
+    results = [];
+    sum = 0;
+    sLength = x.length + y.length - 1;
 
     for (var m = 0; m < sLength; ++m) {
         for (var k = 0; k < sLength; ++k) {
-            if (x[k] != undefined && y[m - k] != undefined) {
+            if (x[k] !== undefined && y[m - k] !== undefined) {
                 sum += x[k] * y[m - k];
             }
         }
         results.push(sum);
         sum = 0;
     }
-        document.getElementById('result').value = results
+    document.getElementById('result').value = results;
 }
